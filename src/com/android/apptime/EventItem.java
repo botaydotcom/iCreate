1d157c64e3895a4f3ff1d4618a12d4e28fb7ae04
Nguyen Hoang Duy <duy.nghoang@gmail.com>
2011-06-19 18:58:26 +0800

    Database commit: Commit time: 002

--- src/com/android/apptime/EventItem.java
+++ src/com/android/apptime/EventItem.java
@@ -7,8 +7,8 @@ import android.graphics.Color;
 
 // REMARK: EventItem *MUST* has [startTime,endTime]
 public class EventItem extends Item{
-	EventItem (String id, String title, String description, String location, List<String> category, List<String> alerttype, String priority, 
-			String itemtype, Date starttime, Date endtime, Date deadline, Date alerttime, Boolean repeat, Boolean completed, Color color)
+	EventItem (String id, String title, String description, String location, String category, List<String> alerttype, String priority, 
+			String itemtype, String starttime, String endtime, String deadline, String alerttime, String repeat, String completed, Integer color)
 			{
 				super(id, title, description,location, category,alerttype, priority, 
 						itemtype, starttime, endtime, deadline, alerttime, repeat, completed, color);
--- src/com/android/apptime/Item.java
+++ src/com/android/apptime/Item.java
@@ -9,19 +9,19 @@ public abstract class Item {
 	public String _location;
 	public String _priority; // LOW-MED-HIGH?
 	public String _itemType; // TaskItem or EventItem?
-	public List<String> _category;
+	public String _category;
 	public List<String> _alertType;
-	public Date _startTime;
-	public Date _endTime;
-	public Date _alertTime; // alert time means sth like : alert 15 min before/ 1hr before / 2hr before
-	public Date _deadline;	
-	public Boolean _repeat;
-	public Boolean _completed;
-	public Color _color;
+	public String _startTime;
+	public String _endTime;
+	public String _alertTime; // alert time means sth like : alert 15 min before/ 1hr before / 2hr before
+	public String _deadline;	
+	public String _repeat;
+	public String _completed;
+	public Integer _color;
 	
 	// Constructor
-	Item (String id, String title, String description, String location, List<String> category, List<String> alerttype, String priority, 
-	String itemtype, Date starttime, Date endtime, Date deadline, Date alerttime, Boolean repeat, Boolean completed, Color color)
+	Item (String id, String title, String description, String location, String category, List<String> alerttype, String priority, 
+	String itemtype, String starttime, String endtime, String deadline, String alerttime, String repeat, String completed, Integer color)
 	{
 		_id = id;
 		_title = title;
@@ -58,7 +58,7 @@ public abstract class Item {
 	{
 		return _location;
 	}
-	public List<String>  GetCategory()
+	public String GetCategory()
 	{
 		return _category;
 	}
@@ -70,33 +70,37 @@ public abstract class Item {
 	{
 		return _priority;
 	}
-	public Date    GetStartTime()
+	public String    GetStartTime()
 	{
 		return _startTime;
 	}
-	public Date    GetEndTime()
+	public String   GetEndTime()
 	{
 		return _endTime;
 	}
-	public Date    GetDeadline()
+	public String   GetDeadline()
 	{
 		return _deadline;
 	}
-	public Date GetAlertTime()
+	public String GetAlertTime()
 	{
 		return _alertTime;
 	}
-	public Boolean GetRepeat()
+	public String GetRepeat()
 	{
 		return _repeat;
 	}
-	public Boolean GetCompleted()
+	public String GetCompleted()
 	{
 		return _completed;
 	}
-	public Color   GetColor()
+	public Integer  GetColor()
 	{
 		return _color;
+	}
+	public String GetItemType()
+	{
+		return _itemType;
 	}
 	
 	// Setters
@@ -116,7 +120,7 @@ public abstract class Item {
 	{
 		_location = newLocation;
 	}
-	public void  SetCategory(List<String> newCategory)
+	public void  SetCategory(String newCategory)
 	{
 		_category = newCategory;
 	}
@@ -128,33 +132,37 @@ public abstract class Item {
 	{
 		_priority = newPriority;
 	}
-	public void  SetStartTime(Date newStartTime)
+	public void  SetStartTime(String newStartTime)
 	{
 		_startTime = newStartTime;
 	}
-	public void  SetEndTime(Date newEndTime)
+	public void  SetEndTime(String newEndTime)
 	{
 		_endTime = newEndTime;
 	}
-	public void  SetDeadline(Date newDeadline)
+	public void  SetDeadline(String newDeadline)
 	{
 		_deadline = newDeadline;
 	}
-	public void  SetAlertTime(Date newAlertTime)
+	public void  SetAlertTime(String newAlertTime)
 	{
 		_alertTime = newAlertTime;
 	}
-	public void  SetRepeat(Boolean newRepeat)
+	public void  SetRepeat(String newRepeat)
 	{
 		_repeat = newRepeat;
 	}
-	public void  SetCompleted(Boolean newCompleted)
+	public void  SetCompleted(String newCompleted)
 	{
 		_completed = newCompleted;
 	}
-	public void  SetColor(Color newColor)
+	public void  SetColor(Integer newColor)
 	{
 		_color = newColor;
+	}
+	public void  SetItemType(String itemType)
+	{
+		_itemType = itemType;
 	}
 }
 
