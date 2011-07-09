19f63970ddfd17c9fd3b08edfc9b59fa4ccf8da1
Nguyen Hoang Duy <duy.nghoang@gmail.com>
2011-07-09 14:52:59 +0800

    conflict

---
+++ src/com/android/apptime/service/IvleDataPuller.java
@@ -0,0 +1,29 @@
+package com.android.apptime.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class IvleDataPuller extends Service{
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Toast.makeText(this,"Service created ...", Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "Service destroyed ...", Toast.LENGTH_LONG).show();
+	}
+
+		
+}
