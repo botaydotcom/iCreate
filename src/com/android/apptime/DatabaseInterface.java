fb593f78ce139013743cabaeccaa729e459a493c
Hoang Tran <hoang.trancong90@gmail.com>
2011-07-09 13:57:50 +0800

    fix database error

--- src/com/android/apptime/DatabaseInterface.java
+++ src/com/android/apptime/DatabaseInterface.java
@@ -5,78 +5,78 @@ import java.util.Date;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 
 import com.android.apptime.database.ItemDBAdapter;
 import com.android.apptime.database.MainDBAdapter;
-
 
 public class DatabaseInterface {
 	// working with database
 	private static DatabaseInterface theInstance = null;
 	private ItemDBAdapter itemdb = null;;
 	private Context context = null;
-	private MainDBAdapter newdb; 
-	private DatabaseInterface(){};
+	private MainDBAdapter newdb;
+
+	private DatabaseInterface() {
+	};
+
 	public DbSetChange dbsetchange;
-	public interface DbSetChange 
-	{
+
+	public interface DbSetChange {
 		public void Update();
 	}
-		
-	public static DatabaseInterface getDatabaseInterface(Context context)
-	{
-		if (theInstance==null) 
-		{
+
+	public static DatabaseInterface getDatabaseInterface(Context context) {
+		if (theInstance == null) {
 			theInstance = new DatabaseInterface();
 		}
 		theInstance.itemdb = new ItemDBAdapter(context);
 		theInstance.context = context;
 		return theInstance;
 	}
-	
-	public void OpenDatabase(Context newcontext)
-	{
+
+	public void OpenDatabase(Context newcontext) {
 		newdb = new MainDBAdapter(newcontext);
-        newdb.open();
+		newdb.open();
 	}
-	
-	public void CloseDatabase()
-	{
+
+	public void CloseDatabase() {
 		newdb.close();
 	}
-	public Item AddItemToDatabase(Context context, Item _item)
-	{
-		itemdb.open();
-		long id = itemdb.createItem(_item);
-		itemdb.close();
-		_item.SetId(String.valueOf(id));
-		dbsetchange.Update();
-		return _item;
+
+	public Item AddItemToDatabase(Context context, Item _item) {
+		try {
+			itemdb.open();
+			long id = itemdb.createItem(_item);
+			itemdb.close();
+			_item.SetId(String.valueOf(id));
+			dbsetchange.Update();
+			return _item;
+		} catch (Exception e) {
+			Log.e("DB", e.getMessage());
+			return null;
+		}
 	}
-	
-	public void RemoveItemFromDatabase(Context context, Item _item)
-	{
-		
+
+	public void RemoveItemFromDatabase(Context context, Item _item) {
+
 		itemdb.open();
 		itemdb.removeItem(Integer.parseInt(_item.GetId()));
 		itemdb.close();
 		dbsetchange.Update();
 	}
-	
-	public void UpdateItemFmomDatabase(Context context, Item _item)
-	{
+
+	public void UpdateItemFmomDatabase(Context context, Item _item) {
 		itemdb.open();
 		itemdb.updateItem(_item);
 		itemdb.close();
 		dbsetchange.Update();
 	}
-	
-	public Item RetrieveItemFromDatabase(Context context, long id)
-	{
+
+	public Item RetrieveItemFromDatabase(Context context, long id) {
 		itemdb.open();
 		Cursor mycs = itemdb.getItemById(id);
-		if (mycs.getString(12).equalsIgnoreCase("Event"))
-		{
+		if (mycs.getString(12).equalsIgnoreCase("Event")) {
 			// return event
 			Cursor eventcursor = mycs;
 			String eid = eventcursor.getString(0);
@@ -93,15 +93,14 @@ public class DatabaseInterface {
 			Long endtime = Long.parseLong(eventcursor.getString(10));
 			Date eendtime = new Date(endtime);
 			Long alerttime = Long.parseLong(eventcursor.getString(11));
-			Date ealerttime = new Date(alerttime); 
+			Date ealerttime = new Date(alerttime);
 			String etype = eventcursor.getString(12);
-			Item newitem = new Item (etitle, edescription, elocation, ecategory, null, epriority,
-					etype, estarttime, eendtime,null,  ealerttime, erepeat, ecompleted, ecolor);
+			Item newitem = new Item(etitle, edescription, elocation, ecategory,
+					null, epriority, etype, estarttime, eendtime, null,
+					ealerttime, erepeat, ecompleted, ecolor);
 			newitem.SetId(eid);
 			return newitem;
-		}
-		else
-		{
+		} else {
 			Cursor eventcursor = mycs;
 			String eid = eventcursor.getString(0);
 			String etitle = eventcursor.getString(1);
@@ -112,35 +111,33 @@ public class DatabaseInterface {
 			String ecategory = eventcursor.getString(6);
 			String ecompleted = eventcursor.getString(7);
 			Integer ecolor = Integer.parseInt(eventcursor.getString(8));
-			
+
 			Long edeadline = Long.parseLong(eventcursor.getString(9));
 			Date deadline = new Date(edeadline);
 			Long alerttime = Long.parseLong(eventcursor.getString(10));
 			Date ealerttime = new Date(alerttime);
 			String etype = eventcursor.getString(11);
-			Item newitem = new Item (etitle, edescription, elocation, ecategory, null, epriority,
-					etype, null, null, deadline,  ealerttime, erepeat, ecompleted, ecolor);
+			Item newitem = new Item(etitle, edescription, elocation, ecategory,
+					null, epriority, etype, null, null, deadline, ealerttime,
+					erepeat, ecompleted, ecolor);
 			newitem.SetId(eid);
 			return newitem;
 		}
-		
+
 	}
-	
-	
+
 	// retrieve item by ITEM id
-	public ArrayList<ArrayList<Item>> RetrieveItemFromDatabase(Context context, Date datetime)
-	{
-		ArrayList<ArrayList<Item>> myitem = new ArrayList<ArrayList<Item>>(); 
+	public ArrayList<ArrayList<Item>> RetrieveItemFromDatabase(Context context,
+			Date datetime) {
+		ArrayList<ArrayList<Item>> myitem = new ArrayList<ArrayList<Item>>();
 		itemdb.open();
 		ArrayList<Cursor> mycs = itemdb.getItemByDate(datetime);
 		itemdb.close();
 		ArrayList<Item> myevent = new ArrayList<Item>();
-		ArrayList<Item> mytask = new ArrayList<Item>(); 
+		ArrayList<Item> mytask = new ArrayList<Item>();
 		Cursor eventcursor = mycs.get(0);
-		if	(eventcursor.moveToFirst())
-		{
-			do
-			{
+		if (eventcursor.moveToFirst()) {
+			do {
 				String eid = eventcursor.getString(0);
 				String etitle = eventcursor.getString(1);
 				String edescription = eventcursor.getString(2);
@@ -155,22 +152,20 @@ public class DatabaseInterface {
 				Long endtime = Long.parseLong(eventcursor.getString(10));
 				Date eendtime = new Date(endtime);
 				Long alerttime = Long.parseLong(eventcursor.getString(11));
-				Date ealerttime = new Date(alerttime); 
+				Date ealerttime = new Date(alerttime);
 				String etype = eventcursor.getString(12);
-				Item newitem = new Item (etitle, edescription, elocation, ecategory, null, epriority,
-						etype, estarttime, eendtime,null,  ealerttime, erepeat, ecompleted, ecolor);
+				Item newitem = new Item(etitle, edescription, elocation,
+						ecategory, null, epriority, etype, estarttime,
+						eendtime, null, ealerttime, erepeat, ecompleted, ecolor);
 				newitem.SetId(eid);
 				myevent.add(newitem);
-				
-				
+
 			} while (eventcursor.moveToNext());
 		}
 		myitem.add(myevent);
 		eventcursor = mycs.get(1);
-		if	(eventcursor.moveToFirst())
-		{
-			do
-			{
+		if (eventcursor.moveToFirst()) {
+			do {
 				String eid = eventcursor.getString(0);
 				String etitle = eventcursor.getString(1);
 				String edescription = eventcursor.getString(2);
@@ -180,39 +175,29 @@ public class DatabaseInterface {
 				String ecategory = eventcursor.getString(6);
 				String ecompleted = eventcursor.getString(7);
 				Integer ecolor = Integer.parseInt(eventcursor.getString(8));
-				
+
 				Long edeadline = Long.parseLong(eventcursor.getString(9));
 				Date deadline = new Date(edeadline);
 				Long alerttime = Long.parseLong(eventcursor.getString(10));
 				Date ealerttime = new Date(alerttime);
 				String etype = eventcursor.getString(11);
-				Item newitem = new Item (etitle, edescription, elocation, ecategory, null, epriority,
-						etype, null, null, deadline,  ealerttime, erepeat, ecompleted, ecolor);
+				Item newitem = new Item(etitle, edescription, elocation,
+						ecategory, null, epriority, etype, null, null,
+						deadline, ealerttime, erepeat, ecompleted, ecolor);
 				newitem.SetId(eid);
 				mytask.add(newitem);
-				
-				
+
 			} while (eventcursor.moveToNext());
 		}
 		myitem.add(mytask);
-		
+
 		return myitem;
-		
+
 	}
-	
-	
-	
-	
-	
-	public void SetObserver(DbSetChange dbsetchange)
-	{
+
+	public void SetObserver(DbSetChange dbsetchange) {
 		this.dbsetchange = dbsetchange;
-		
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
--- src/com/android/apptime/database/MainDBAdapter.java
+++ src/com/android/apptime/database/MainDBAdapter.java
@@ -4,189 +4,170 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class MainDBAdapter {
 	public static final String DATABASE_NAME = "appTimeDBB1.db"; //$NON-NLS-1$
 
-    public static final int DATABASE_VERSION = 1;
-    
-    // TASK TABLE
-    private static final String DATABASE_TABLE_TASK = "Tasks_Table";
-    private static final String TASKDB_KEY_ID = "taskdb_task_id";
-    private static final String TASKDB_TITLE = "taskdb_task_title";
-    private static final String TASKDB_DESCRIPTION = "taskdb_description";
-    private static final String TASKDB_LOCATION = "taskdb_location";
-    private static final String TASKDB_PRIORITY = "taskdb_priority";
-    private static final String TASKDB_REPEAT = "taskdb_repeat";
-    private static final String TASKDB_CATEGORY = "taskdb_category";
-    private static final String TASKDB_COMPLETED = "taskdb_completed";
-    private static final String TASKDB_COLOR = "taskdb_color";
-    private static final String TASKDB_DEADLINE = "taskdb_deadline";
-    private static final String TASKDB_ALERTTIME = "taskdb_alert_time";
-    private static final String TASKDB_TYPE = "taskdb_type";
-    // EVENT TABLE
-    private static final String DATABASE_TABLE_EVENT = "Events_Table";
-    private static final String EVENTDB_KEY_ID = "eventdb_event_id";
-    private static final String EVENTDB_TITLE = "eventdb_event_title";
-    private static final String EVENTDB_DESCRIPTION = "eventdb_description";
-    private static final String EVENTDB_LOCATION = "eventdb_location";
-    private static final String EVENTDB_PRIORITY = "eventdb_priority";
-    private static final String EVENTDB_REPEAT = "eventdb_repeat";
-    private static final String EVENTDB_CATEGORY = "eventdb_category";
-    private static final String EVENTDB_COMPLETED = "eventdb_completed";
-    private static final String EVENTDB_COLOR = "eventdb_color";
-    private static final String EVENTDB_STARTTIME = "eventdb_start_time";
-    private static final String EVENTDB_ENDTIME = "eventdb_end_time";
-    private static final String EVENTDB_ALERTTIME = "eventdb_alert_time";
-    private static final String EVENTDB_TYPE = "eventdb_type"; 
-    // ITEM TABLE 
-    private static final String DATABASE_TABLE_ITEM = "Items_Table";
-    private static final String ITEMDB_KEY_ID = "itemdb_itemid";
-    private static final String ITEMDB_KEY_TASK = "itemdb_taskid";
-    private static final String ITEMDB_KEY_EVENT = "itemdb_eventid";
-    
-    // CATEGORY TABLE
-    private static final String DATABASE_TABLE_CATEGORY = "Categories_Table";
-    private static final String CATEGORYDB_KEY_ID = "categorydb_category_id";
-    private static final String CATEGORYDB_TITLE = "categorydb_title";
-    
-    // ALERTTYPE TABLE
-    private static final String DATABASE_TABLE_ALERTTYPE = "Alerttype_Table";
-    private static final String ALERTTYPEDB_KEY_ID = "alerttypedb_alerttype_id";
-    private static final String ALERTTYPEDB_TITLE = "alerttypedb_title";
-    
- // MAP TABLE
-    private static final String DATABASE_TABLE_MAP = "Map_Table";
-    private static final String MAPDB_KEY_ID = "mapdb_loc_id";
-    private static final String MAPDB_TITLE = "map_title";
-    private static final String MAPDB_HORIZONTAL = "mapdb_horizontal";
-    private static final String MAPDB_VERTICAL = "map_vertical";
-    private static final String MAPDB_LONGITUDE = "mapdb_longitude";
-    private static final String MAPDB_LATITUDE = "map_latitude";
-    private static final String MAPDB_LINK = "map_link";
-    
-    /*
-     * Map SQL commands  
-     */
-    
-    // SQL COMMAND TO CREATE MAP TABLE
-    private static final String DATABASE_CREATE_MAP = "create table " + DATABASE_TABLE_MAP
-    + " (" + MAPDB_KEY_ID + " integer primary key autoincrement, " + 
-    MAPDB_TITLE + " text, " +
-    MAPDB_HORIZONTAL + " text not null " +
-    MAPDB_VERTICAL + " text not null " +
-    MAPDB_LONGITUDE + " text not null " +
-    MAPDB_LATITUDE + " text not null, " +
-    MAPDB_LINK + " text" + ");";
-    
-    
-    
-    
-    
-    // SQL COMMAND TO CREATE TASK TABLE
-    private static final String DATABASE_CREATE_TASK = "create table " + DATABASE_TABLE_TASK
-    + " (" + TASKDB_KEY_ID + " integer primary key autoincrement, " + 
-    TASKDB_TITLE + " text, " +
-    TASKDB_DESCRIPTION + " text, " +
-    TASKDB_LOCATION + " text, " +
-    TASKDB_PRIORITY + " text, " +
-    TASKDB_REPEAT + " text not null, " +
-    TASKDB_CATEGORY + " text, " +
-    TASKDB_COMPLETED + " text not null, " +
-    TASKDB_COLOR + " integer not null, " +
-    TASKDB_DEADLINE + " real not null, " +
-    TASKDB_ALERTTIME + " real not null, " +  
-    TASKDB_TYPE + " text not null" + ");";
-    
-    
-    // SQL COMMAND TO CREATE EVENT TABLE
-    private static final String DATABASE_CREATE_EVENT = "create table " + DATABASE_TABLE_EVENT
-    + " (" + EVENTDB_KEY_ID + " integer primary key autoincrement, " + 
-    EVENTDB_TITLE + " text, " +
-    EVENTDB_DESCRIPTION + " text, " +
-    EVENTDB_LOCATION + " text, " +
-    EVENTDB_PRIORITY + " text, " +
-    EVENTDB_REPEAT + " text not null, " +
-    EVENTDB_CATEGORY + " text, " +
-    EVENTDB_COMPLETED + " text not null, " +
-    EVENTDB_COLOR + " integer not null, " +
-    EVENTDB_STARTTIME + " real not null, " +
-    EVENTDB_ENDTIME + " real not null, " +
-    EVENTDB_ALERTTIME + " real not null," + 
-    EVENTDB_TYPE + " text not null" + ");";
-    
-    
-    
-    // SQL COMMAND TO CREATE ITEM TABLE
-    private static final String DATABASE_CREATE_ITEM = "create table " + 
-    DATABASE_TABLE_ITEM + " (" + ITEMDB_KEY_ID + " integer primary key autoincrement, " +
-    ITEMDB_KEY_TASK + " integer not null, " +  ITEMDB_KEY_EVENT + " integer not null," + " FOREIGN KEY ("+ ITEMDB_KEY_TASK +") REFERENCES " + DATABASE_TABLE_TASK + " ("+ TASKDB_KEY_ID +"), "
-    + " FOREIGN KEY ("+ ITEMDB_KEY_EVENT +") REFERENCES " + DATABASE_TABLE_EVENT + " ("+ EVENTDB_KEY_ID +"));";
+	public static final int DATABASE_VERSION = 1;
 
+	// TASK TABLE
+	private static final String DATABASE_TABLE_TASK = "Tasks_Table";
+	private static final String TASKDB_KEY_ID = "taskdb_task_id";
+	private static final String TASKDB_TITLE = "taskdb_task_title";
+	private static final String TASKDB_DESCRIPTION = "taskdb_description";
+	private static final String TASKDB_LOCATION = "taskdb_location";
+	private static final String TASKDB_PRIORITY = "taskdb_priority";
+	private static final String TASKDB_REPEAT = "taskdb_repeat";
+	private static final String TASKDB_CATEGORY = "taskdb_category";
+	private static final String TASKDB_COMPLETED = "taskdb_completed";
+	private static final String TASKDB_COLOR = "taskdb_color";
+	private static final String TASKDB_DEADLINE = "taskdb_deadline";
+	private static final String TASKDB_ALERTTIME = "taskdb_alert_time";
+	private static final String TASKDB_TYPE = "taskdb_type";
+	// EVENT TABLE
+	private static final String DATABASE_TABLE_EVENT = "Events_Table";
+	private static final String EVENTDB_KEY_ID = "eventdb_event_id";
+	private static final String EVENTDB_TITLE = "eventdb_event_title";
+	private static final String EVENTDB_DESCRIPTION = "eventdb_description";
+	private static final String EVENTDB_LOCATION = "eventdb_location";
+	private static final String EVENTDB_PRIORITY = "eventdb_priority";
+	private static final String EVENTDB_REPEAT = "eventdb_repeat";
+	private static final String EVENTDB_CATEGORY = "eventdb_category";
+	private static final String EVENTDB_COMPLETED = "eventdb_completed";
+	private static final String EVENTDB_COLOR = "eventdb_color";
+	private static final String EVENTDB_STARTTIME = "eventdb_start_time";
+	private static final String EVENTDB_ENDTIME = "eventdb_end_time";
+	private static final String EVENTDB_ALERTTIME = "eventdb_alert_time";
+	private static final String EVENTDB_TYPE = "eventdb_type";
+	// ITEM TABLE
+	private static final String DATABASE_TABLE_ITEM = "Items_Table";
+	private static final String ITEMDB_KEY_ID = "itemdb_itemid";
+	private static final String ITEMDB_KEY_TASK = "itemdb_taskid";
+	private static final String ITEMDB_KEY_EVENT = "itemdb_eventid";
+
+	// CATEGORY TABLE
+	private static final String DATABASE_TABLE_CATEGORY = "Categories_Table";
+	private static final String CATEGORYDB_KEY_ID = "categorydb_category_id";
+	private static final String CATEGORYDB_TITLE = "categorydb_title";
+
+	// ALERTTYPE TABLE
+	private static final String DATABASE_TABLE_ALERTTYPE = "Alerttype_Table";
+	private static final String ALERTTYPEDB_KEY_ID = "alerttypedb_alerttype_id";
+	private static final String ALERTTYPEDB_TITLE = "alerttypedb_title";
+
+	// MAP TABLE
+	private static final String DATABASE_TABLE_MAP = "Map_Table";
+	private static final String MAPDB_KEY_ID = "mapdb_loc_id";
+	private static final String MAPDB_TITLE = "map_title";
+	private static final String MAPDB_HORIZONTAL = "mapdb_horizontal";
+	private static final String MAPDB_VERTICAL = "map_vertical";
+	private static final String MAPDB_LONGITUDE = "mapdb_longitude";
+	private static final String MAPDB_LATITUDE = "map_latitude";
+	private static final String MAPDB_LINK = "map_link";
+
+	/*
+	 * Map SQL commands
+	 */
+
+	// SQL COMMAND TO CREATE MAP TABLE
+	private static final String DATABASE_CREATE_MAP = "create table "
+			+ DATABASE_TABLE_MAP + " (" + MAPDB_KEY_ID
+			+ " integer primary key autoincrement, " + MAPDB_TITLE + " text, "
+			+ MAPDB_HORIZONTAL + " text not null, " + MAPDB_VERTICAL
+			+ " text not null, " + MAPDB_LONGITUDE + " text not null, "
+			+ MAPDB_LATITUDE + " text not null, " + MAPDB_LINK + " text" + ");";
+
+	// SQL COMMAND TO CREATE TASK TABLE
+	private static final String DATABASE_CREATE_TASK = "create table "
+			+ DATABASE_TABLE_TASK + " (" + TASKDB_KEY_ID
+			+ " integer primary key autoincrement, " + TASKDB_TITLE + " text, "
+			+ TASKDB_DESCRIPTION + " text, " + TASKDB_LOCATION + " text, "
+			+ TASKDB_PRIORITY + " text, " + TASKDB_REPEAT + " text not null, "
+			+ TASKDB_CATEGORY + " text, " + TASKDB_COMPLETED
+			+ " text not null, " + TASKDB_COLOR + " integer not null, "
+			+ TASKDB_DEADLINE + " integer not null, " + TASKDB_ALERTTIME
+			+ " integer not null, " + TASKDB_TYPE + " text not null" + ");";
+
+	// SQL COMMAND TO CREATE EVENT TABLE
+	private static final String DATABASE_CREATE_EVENT = "create table "
+			+ DATABASE_TABLE_EVENT + " (" + EVENTDB_KEY_ID
+			+ " integer primary key autoincrement, " + EVENTDB_TITLE
+			+ " text, " + EVENTDB_DESCRIPTION + " text, " + EVENTDB_LOCATION
+			+ " text, " + EVENTDB_PRIORITY + " text, " + EVENTDB_REPEAT
+			+ " text not null, " + EVENTDB_CATEGORY + " text, "
+			+ EVENTDB_COMPLETED + " text not null, " + EVENTDB_COLOR
+			+ " integer not null, " + EVENTDB_STARTTIME + " integer not null, "
+			+ EVENTDB_ENDTIME + " integer not null, " + EVENTDB_ALERTTIME
+			+ " integer not null," + EVENTDB_TYPE + " text not null" + ");";
+
+	// SQL COMMAND TO CREATE ITEM TABLE
+	private static final String DATABASE_CREATE_ITEM = "create table "
+			+ DATABASE_TABLE_ITEM + " (" + ITEMDB_KEY_ID
+			+ " integer primary key autoincrement, " + ITEMDB_KEY_TASK
+			+ " integer not null, " + ITEMDB_KEY_EVENT + " integer not null,"
+			+ " FOREIGN KEY (" + ITEMDB_KEY_TASK + ") REFERENCES "
+			+ DATABASE_TABLE_TASK + " (" + TASKDB_KEY_ID + "), "
+			+ " FOREIGN KEY (" + ITEMDB_KEY_EVENT + ") REFERENCES "
+			+ DATABASE_TABLE_EVENT + " (" + EVENTDB_KEY_ID + "));";
 
 	// SQL COMMAND TO CREATE CATEGORY TABLE
-    private static final String DATABASE_CREATE_CATEGORY = "create table " +
-    DATABASE_TABLE_CATEGORY + " (" + CATEGORYDB_KEY_ID + " integer primary key autoincrement, " +
-    CATEGORYDB_TITLE + " text not null);";
-    
- // SQL COMMAND TO CREATE ALERTTYPE TABLE
-    private static final String DATABASE_CREATE_ALERTTYPE = "create table " +
-    DATABASE_TABLE_ALERTTYPE + " (" + ALERTTYPEDB_KEY_ID + " integer primary key autoincrement, " +
-    ALERTTYPEDB_TITLE + " text not null);";
-    
+	private static final String DATABASE_CREATE_CATEGORY = "create table "
+			+ DATABASE_TABLE_CATEGORY + " (" + CATEGORYDB_KEY_ID
+			+ " integer primary key autoincrement, " + CATEGORYDB_TITLE
+			+ " text not null);";
 
-    private final Context context; 
-    private DatabaseHelper DBHelper;
-    private SQLiteDatabase db;
-    
-    public MainDBAdapter(Context ctx)
-    {
-        this.context = ctx;
-        this.DBHelper = new DatabaseHelper(this.context);
-    }
+	// SQL COMMAND TO CREATE ALERTTYPE TABLE
+	private static final String DATABASE_CREATE_ALERTTYPE = "create table "
+			+ DATABASE_TABLE_ALERTTYPE + " (" + ALERTTYPEDB_KEY_ID
+			+ " integer primary key autoincrement, " + ALERTTYPEDB_TITLE
+			+ " text not null);";
 
-    private static class DatabaseHelper extends SQLiteOpenHelper 
-    {
-        DatabaseHelper(Context context) 
-        {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+	private final Context context;
+	private DatabaseHelper DBHelper;
+	private SQLiteDatabase db;
 
-        @Override
-        public void onCreate(SQLiteDatabase db) 
-        {
-            db.execSQL(DATABASE_CREATE_TASK);
-            db.execSQL(DATABASE_CREATE_EVENT);
-            db.execSQL(DATABASE_CREATE_ITEM);  
-            db.execSQL(DATABASE_CREATE_CATEGORY);
-            db.execSQL(DATABASE_CREATE_ALERTTYPE);
-            db.execSQL(DATABASE_CREATE_MAP);
-        }
+	public MainDBAdapter(Context ctx) {
+		this.context = ctx;
+		this.DBHelper = new DatabaseHelper(this.context);
+	}
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, 
-        int newVersion) 
-        {               
-            // Adding any table mods to this guy here
-        }
-    } 
-	
-    public MainDBAdapter open() throws SQLException 
-    {
-        this.db = this.DBHelper.getWritableDatabase();
-        return this;
-    }
+	private static class DatabaseHelper extends SQLiteOpenHelper {
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
 
-    /**
-     * close the db 
-     * return type: void
-     */
-    public void close() 
-    {
-        this.DBHelper.close();
-    }
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE_TASK);
+			db.execSQL(DATABASE_CREATE_EVENT);
+			db.execSQL(DATABASE_CREATE_ITEM);
+			db.execSQL(DATABASE_CREATE_CATEGORY);
+			db.execSQL(DATABASE_CREATE_ALERTTYPE);
+			db.execSQL(DATABASE_CREATE_MAP);
+		}
 
-	
-	
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// Adding any table mods to this guy here
+		}
+	}
+
+	public MainDBAdapter open() throws SQLException {
+		try {
+			this.db = this.DBHelper.getWritableDatabase();
+			return this;
+		} catch (Exception e) {
+			Log.d("DB", e.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * close the db return type: void
+	 */
+	public void close() {
+		this.DBHelper.close();
+	}
+
 }
-
